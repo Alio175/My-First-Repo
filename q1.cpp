bccc001bad49c27c287a47480fa105e33fec5ebe
Ali Akbar Khan <aliakbarkhan@makethehack.com>
2022-11-26 17:43:52 +0000

    Added trinagle

---
+++ q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+
+
+int main()
+{
+    for(int i  = 0 ; i < 1 ; i++)
+    {
+        for(int j = 0 ; j <  8 ; j++)
+        {
+            cout<<"*";
+        }
+    }
+
+return 0 ; 
+
+}
